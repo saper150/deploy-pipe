4d0a72c7e9efba77830a1abf61b2f83e3b134fde
Marcin Wydrzyński <wydrzynskimarcin@gmail.com>
2018-03-09 18:52:24 +0100

    removed semicoln

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const ssh = new (require('node-ssh'))()
 const path = require('path')
 
-const util = require('util');
-const exec = util.promisify(require('child_process').exec);
+const util = require('util')
+const exec = util.promisify(require('child_process').exec)
 
 module.exports = async (conObj, { remoteWorkingDir }, pipe) => {
 
